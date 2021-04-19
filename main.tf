13ec9adfc6dae37c0610f99fdab56fa6af60de1b
Ubuntu <bryanchanngc@bryanchanngc.f0s2dyeyfrpedicvr4wksnswth.gx.internal.cloudapp.net>
2021-04-19 05:48:11 +0000

    Add files

---
+++ main.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "default"
+  region                  = "us-east-1"
+}
+
+
+resource "tls_private_key" "private-key" {
+  algorithm   = "RSA"
+  rsa_bits    = 2048
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${terraform.workspace}.deployer-key"
+  public_key = tls_private_key.private-key.public_key_openssh
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_security_group" "student-sg" {
+  name = "Standard-SG"
+  description = "Student security group"
+
+  tags = {
+    Name = "Standard-SG"
+    Environment = terraform.workspace
+  }
+}
+
+resource "aws_security_group_rule" "create-sgr-ssh" {
+  security_group_id = aws_security_group.student-sg.id
+  cidr_blocks         = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+  type              = "ingress"
+  self              = false
+}
+
+resource "aws_security_group_rule" "create-sgr-jenkins" {
+  security_group_id = aws_security_group.student-sg.id
+  cidr_blocks         = ["0.0.0.0/0"]
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
+  type              = "ingress"
+  self              = false
+}
+
+resource "aws_security_group_rule" "create-sgr-outbound" {
+  security_group_id = aws_security_group.student-sg.id
+  cidr_blocks         = ["0.0.0.0/0"]
+  from_port         = 0
+  protocol          = "all"
+  to_port           = 65535
+  type              = "egress"
+  self              = false
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = ["Standard-SG"]
+  tags = {
+    Name = "ClientServer"
+  }
+
+  provisioner "local-exec" {
+    command = "terraform output private-key | sed '1d' | sed '28d' | sed '28d' > ~/.ssh/client-server.pem; chmod 600 ~/.ssh/client-server.pem"
+  }
+
+  provisioner "local-exec" {
+    command = "terraform output aws_instance.this.*.public_dns  | sed '1d' | sed '28d' | sed '28d' > ./hosts"
+  }
+
+}
